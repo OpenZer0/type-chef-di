6e09b30e93f5b6cb1eb7119fec9504df2fac82e9
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-11-07 21:41:36 +0100

    fix: add InjectProperty types

--- lib/decorators/InjectProperty.ts
+++ lib/decorators/InjectProperty.ts
@@ -1,7 +1,9 @@
 import { Keys } from "../Keys";
 
-export function InjectProperty(key: string) {
-    return function inject(target: object, propertyKey: any) {
+export function InjectProperty<I = any>(key: string) {
+    return function inject<
+        T extends  Record<K, I>,
+        K extends string>(target: T, propertyKey: K) {
         const ctr = target.constructor;
         const metadata: any = Reflect.getMetadata(Keys.PROPERTY_INJECT_KEY, ctr) || {};
         metadata[propertyKey] = key;
